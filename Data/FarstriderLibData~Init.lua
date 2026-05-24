e063a9ed47f21bde44234634c5f9bab6fbc200ec
Deathwing <deathwing4528@googlemail.com>
2026-05-24 19:48:42 +0200

    1.3.2

--- Data/FarstriderLibData~Init.lua
+++ Data/FarstriderLibData~Init.lua
@@ -1,7 +1,7 @@
 -- FarstriderLibData~Init.lua
 -- local _, FarstriderLibData = ...
 
-local VERSION = 10401
+local VERSION = 10402
 
 if FarstriderLibData_API and (FarstriderLibData_API.VERSION or 0) >= VERSION then return end
 
